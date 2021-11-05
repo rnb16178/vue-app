83e7023ce1ed2feb3a1593d3dc9bb9ff17e23251
Ross McMillan <73564091+rnb16178@users.noreply.github.com>
2021-11-05 00:01:57 +0000

    cors

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
+import AxiosPlugin from 'vue-axios-cors';
 import router from "./router";
 import vuetify from '@/plugins/vuetify' // path to vuetify export
 import BootstrapVue from "bootstrap-vue";
@@ -9,7 +10,7 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
-
+Vue.use(AxiosPlugin)
 
 new Vue({
   vuetify,
